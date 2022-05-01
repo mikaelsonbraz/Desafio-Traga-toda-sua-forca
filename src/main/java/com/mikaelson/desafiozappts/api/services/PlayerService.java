6f72096d988af8b7fd0e4d15a41e8b11ccc56aaa
Mikaelson <mikaelsonmrn@gmail.com>
2022-04-30 21:26:42 -0300

    Player services tested and passed

---
+++ src/main/java/com/mikaelson/desafiozappts/api/services/PlayerService.java
@@ -0,0 +1,17 @@
+package com.mikaelson.desafiozappts.api.services;
+
+import com.mikaelson.desafiozappts.api.models.entites.Player;
+
+import java.util.Optional;
+
+public interface PlayerService {
+
+    Player save(Player player);
+
+    Optional<Player> getById(Integer id);
+
+    Player update(Player player);
+
+    void delete(Player player);
+
+}
